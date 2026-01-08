ca54e1b85077135096d723eeb44cf7e9c1fa041b
karimireddy7 <karimireddy7674@gmail.com>
2026-01-08 17:55:10 +0530

    Update main.tf

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,1 +1,22 @@
+resource "google_compute_network" "vpc" {
+  name                    = var.vpc_name
+  auto_create_subnetworks = false
+}
 
+resource "google_compute_subnetwork" "subnet" {
+  name          = "private-subnet"
+  region        = var.region
+  network       = google_compute_network.vpc.id
+  ip_cidr_range = var.subnet_cidr
+}
+
+resource "google_compute_router" "router" {
+  name    = "nat-router"
+  region  = var.region
+  network = google_compute_network.vpc.id
+}
+
+resource "google_compute_router_nat" "nat" {
+  name                               = "cloud-nat"
+  router                             = google_compute_router.router.name
+  region
