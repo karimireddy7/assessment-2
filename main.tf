58c733a2140a17b99c3f2752162aa4f347118668
karimireddy7 <karimireddy7674@gmail.com>
2026-01-08 17:54:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,22 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
 
+module "network" {
+  source       = "./modules/network"
+  region       = var.region
+  vpc_name     = "private-vpc"
+  subnet_cidr  = "10.10.0.0/24"
+}
+
+module "vm" {
+  source    = "./modules/vm"
+  region    = var.region
+  subnet_id = module.network.subnet_id
+}
+
+module "lb" {
+  source         = "./modules/lb"
+  instance_group = module.vm.instance_group
+}
